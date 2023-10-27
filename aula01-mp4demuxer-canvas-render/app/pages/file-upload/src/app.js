fa050664ac149435e2d7b9e1bf01f1d9311787e6
notebook samsung dev-celo <marcelohenrique.backend@gmail.com>
2023-10-27 20:15:59 -0300

    feat: min 43 trabalhando no worker.

--- aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/app.js
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/app.js
@@ -3,18 +3,26 @@ import View from './view.js';
 
 const view = new View()
 const clock = new Clock()
+const worker = new Worker('./src/worker/worker.js', {
+    type: 'module'
+})
+
+worker.onmessage = ({ data }) => {
+    if (data.status !== 'done') return;
+    clock.stop()
+    view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+}
 
 let took = ''
 view.configureOnFileChange((file) => {
+    worker.postMessage({
+        file
+    })
+
     clock.start((time) => {
         took = time;
         view.updateElapsedTime(`Process started ${time}`)
     })
-
-    setTimeout(() => {
-        clock.stop()
-        view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
-    }, 5000)
 })
 
 // modelo de fetch fake para imitar iteração do user
@@ -25,7 +33,6 @@ async function fakeFetch() {
     })
 
     // trás o tamanho do arquivo
-    console.log(response.headers.get('content-length'));
     const file = new File([await response.blob()], filePath, {
         type: "video/mp4",
         lastModified: Date.now()
---
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,8 @@
+onmessage = ({ data }) => {
+    debugger
+    setTimeout(() => {
+        self.postMessage({
+            status: 'done',
+        })
+    }, 2000)
+}
